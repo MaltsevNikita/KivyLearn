55cf30b014ec18d443fc2d329a5a6ccd4627e2e6
unknown <mn121288@gmail.com>
2019-07-01 08:26:16 +0400

    Коммит первого урока

--- main.py
+++ main.py
@@ -1,25 +1,31 @@
-from kivy.app import App
-from kivy.uix.button import Button
-from kivy.uix.codeinput import CodeInput
-from pygments.lexers import HtmlLexer
+from kivy.app import App # подключаем модуль kivy
+from kivy.uix.button import Button # подключаем модуль uix - кнопка
+from kivy.uix.codeinput import CodeInput # подключаем модуль для добавления виджета "редактор кода"
+from pygments.lexers import HtmlLexer # подклюаем модуль подсветки синтаксиса
 
+# Создаём эземплары классов================================
 from kivy.config import Config
 
 from kivy.uix.floatlayout import FloatLayout
 
 from kivy.uix.scatter import Scatter
+# =========================================================
 
+# Конфигурация окна приложения=============================
 Config.set('graphics', 'resizable', 0)
 Config.set('graphics', 'width', 640)
 Config.set('graphics', 'height', 480)
+# =========================================================
 
+# Создаём и описываем основной класс=======================
 class MyApp(App):
 	def build(self):
+		# Создаём модуль scatter для поворота экрана 2 прикосновениями
 		s = Scatter()
-
+# Содаём FlostLayout, чтобы элементы выстроились в одну линию (wdgh,height)
 		fl = FloatLayout(size = (300,300))
-		s.add_widget(fl)
-		fl.add_widget(Button(text = "Это моя первая кнопка", 
+		s.add_widget(fl) # добавляем виджет в объект scatter
+		fl.add_widget(Button(text = "Это моя первая кнопка", # Добавляем виджет кнопки в объект FloatLayout
 			font_size = 16,
 			on_press = self.btn_press,
 			background_color = [1,0,0,1],
@@ -28,11 +34,13 @@ class MyApp(App):
 			pos = (640/2 - 160,0)) , 480/2);
 
 		return s
-
+# описываем функцию - обработчик в теле класса MyApp
+	
 	def btn_press(self, instance):
+
 		print("Кнопка нажата")
 		instance.text = "Hello world"
-		 
 
-if __name__ =="__main__":
+
+if __name__ == "__main__":
 	MyApp().run()
