431991fc40119d61619b9f6919db8cc02eb0f818
unknown <mn121288@gmail.com>
2019-07-01 17:19:47 +0400

    сделали заготовку для калькулятора

--- main.py
+++ main.py
@@ -1,21 +1,33 @@
 from kivy.app import App # подключаем модуль kivy
 from kivy.uix.button import Button
-from kivy.uix.textinput import TextInput
+from kivy.uix.widget import Widget
 
-from kivy.uix.boxlayout import BoxLayout
-from kivy.uix.anchorlayout import AnchorLayout
+from kivy.uix.gridlayout import GridLayout
 
 class BoxApp(App):
 	def build(self):
-		al = AnchorLayout()
-		bl = BoxLayout(orientation = 'vertical', size_hint=[.4, .4])
+		gl = GridLayout(cols=4, padding=[35],spacing=[3])
 
-		bl.add_widget(TextInput())
-		bl.add_widget(TextInput())
-		bl.add_widget(Button(text="Enter"))
+		gl.add_widget( Button(text='7') )
+		gl.add_widget( Button(text='8') )
+		gl.add_widget( Button(text='9') )
+		gl.add_widget( Button(text='X') )
 
-		al.add_widget(bl)
-		return al
+		gl.add_widget( Button(text='4') )
+		gl.add_widget( Button(text='5') )
+		gl.add_widget( Button(text='6') )
+		gl.add_widget( Button(text='-') )
+
+		gl.add_widget( Button(text='1') )
+		gl.add_widget( Button(text='2') )
+		gl.add_widget( Button(text='3') )
+		gl.add_widget( Button(text='+') )
+
+		gl.add_widget( Widget())
+		gl.add_widget( Button(text='0') )
+		gl.add_widget( Button(text='.') )
+		gl.add_widget( Button(text='=') )
+		return gl
 # следующей строкой мы указываем, что приложение было запущено как самостоятельное
 # приложение, а не кк какой-то отдельный модуль
 
