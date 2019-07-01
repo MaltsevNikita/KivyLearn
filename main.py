dad0f85ece9fb5316b9aa1d4cae9bcf66b446c5b
unknown <mn121288@gmail.com>
2019-07-01 17:02:30 +0400

    Второй урок. Разобрали box layout, gridLayout и anchorLayout

--- main.py
+++ main.py
@@ -1,17 +1,21 @@
 from kivy.app import App # подключаем модуль kivy
 from kivy.uix.button import Button
+from kivy.uix.textinput import TextInput
 
-
+from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.anchorlayout import AnchorLayout
 
 class BoxApp(App):
 	def build(self):
+		al = AnchorLayout()
+		bl = BoxLayout(orientation = 'vertical', size_hint=[.4, .4])
 
-		bl = AnchorLayout(anchor_x = 'left',anchor_y = 'top')
+		bl.add_widget(TextInput())
+		bl.add_widget(TextInput())
+		bl.add_widget(Button(text="Enter"))
 
-		bl.add_widget(Button(text="Button" , size_hint = [.5, .5]))
-
-		return bl
+		al.add_widget(bl)
+		return al
 # следующей строкой мы указываем, что приложение было запущено как самостоятельное
 # приложение, а не кк какой-то отдельный модуль
 
