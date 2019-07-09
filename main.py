1cd9ad27120174f1aac78af7792c041d9407009b
unknown <mn121288@gmail.com>
2019-07-09 14:38:54 +0400

    Компилится. иду дальше

--- main.py
+++ main.py
@@ -1,36 +1,41 @@
-from kivy.app import App # подключаем модуль kivy
+from kivy.app import App
+
 from kivy.uix.button import Button
 from kivy.uix.widget import Widget
+from kivy.uix.label import Label
 
 from kivy.uix.gridlayout import GridLayout
+from kivy.uix.boxlayout import BoxLayout
 
-class BoxApp(App):
+class CalculatorApp(App):
 	def build(self):
-		gl = GridLayout(cols=4, padding=[35],spacing=[3])
+		bl = BoxLayout(orientation='vertical')
+		gl = GridLayout(cols = 4)
 
-		gl.add_widget( Button(text='7') )
-		gl.add_widget( Button(text='8') )
-		gl.add_widget( Button(text='9') )
-		gl.add_widget( Button(text='X') )
+		bl.add_widget(Label(text = '0'))
+		
+		gl.add_widget( Button(text="7"))
+		gl.add_widget( Button(text="8"))
+		gl.add_widget( Button(text="9"))
+		gl.add_widget( Button(text="*"))
 
-		gl.add_widget( Button(text='4') )
-		gl.add_widget( Button(text='5') )
-		gl.add_widget( Button(text='6') )
-		gl.add_widget( Button(text='-') )
+		gl.add_widget( Button(text="6"))
+		gl.add_widget( Button(text="5"))
+		gl.add_widget( Button(text="4"))
+		gl.add_widget( Button(text="+"))
 
-		gl.add_widget( Button(text='1') )
-		gl.add_widget( Button(text='2') )
-		gl.add_widget( Button(text='3') )
-		gl.add_widget( Button(text='+') )
+		gl.add_widget( Button(text="1"))
+		gl.add_widget( Button(text="2"))
+		gl.add_widget( Button(text="3"))
+		gl.add_widget( Button(text="+"))
 
 		gl.add_widget( Widget())
-		gl.add_widget( Button(text='0') )
-		gl.add_widget( Button(text='.') )
-		gl.add_widget( Button(text='=') )
-		return gl
-# следующей строкой мы указываем, что приложение было запущено как самостоятельное
-# приложение, а не кк какой-то отдельный модуль
+		gl.add_widget( Button(text="0"))
+		gl.add_widget( Button(text="."))
+		gl.add_widget( Button(text="="))
 
+		bl.add_widget(gl)
+		return bl
 
 if __name__ == "__main__":
-	BoxApp().run()
+	CalculatorApp().run()
