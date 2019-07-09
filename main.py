5f8f542fbb79d37d5ed5ccb3157e155477499394
unknown <mn121288@gmail.com>
2019-07-09 16:03:34 +0400

    Поправил отображние вводимых значений на label. Далее надо добавить обрботчик кнпки равно

--- main.py
+++ main.py
@@ -9,35 +9,53 @@ from kivy.uix.boxlayout import BoxLayout
 
 from kivy.config import Config
 Config.set('graphics', 'resizable',0)
-Config.set('graphics', 'wigh',400)
-Config.set('graphics', 'height',500)
+Config.set('graphics', 'width', 400)
+Config.set('graphics', 'height', 500)
 
 class CalculatorApp(App):
+	def update_label(self):
+		self.lbl.text = self.formula
+
+	def add_number(self, instance):
+		if (self.formula == "0"):
+			self.formula = ""
+
+		self.formula += str(instance.text)
+		self.update_label()
+	
+	def add_operation(self,instance):
+		self.formula += str(instance.text)
+		self.update_label()
+
 	def build(self):
+		self.formula = ""
 		bl = BoxLayout(orientation='vertical', padding = 25)
-		gl = GridLayout(cols = 4, spacing = 3)
+		gl = GridLayout(cols = 4, spacing = 3, size_hint = (1, .6))
 
-		bl.add_widget(Label(text = '0'))
-		
-		gl.add_widget( Button(text="7"))
-		gl.add_widget( Button(text="8"))
-		gl.add_widget( Button(text="9"))
-		gl.add_widget( Button(text="*"))
+		self.lbl = Label(text = '0', font_size = 40, halign="right", 
+							valign = "center", size_hint = (1, .4), text_size = (400 - 50, 500 * .4 - 50))
 
-		gl.add_widget( Button(text="6"))
-		gl.add_widget( Button(text="5"))
-		gl.add_widget( Button(text="4"))
-		gl.add_widget( Button(text="+"))
+		bl.add_widget( self.lbl )
 
-		gl.add_widget( Button(text="1"))
-		gl.add_widget( Button(text="2"))
-		gl.add_widget( Button(text="3"))
-		gl.add_widget( Button(text="+"))
+		gl.add_widget( Button(text="7",on_press = self.add_number))
+		gl.add_widget( Button(text="8",on_press = self.add_number))
+		gl.add_widget( Button(text="9",on_press = self.add_number))
+		gl.add_widget( Button(text="*",on_press = self.add_operation) )
+
+		gl.add_widget( Button(text="4",on_press = self.add_number))
+		gl.add_widget( Button(text="5",on_press = self.add_number))
+		gl.add_widget( Button(text="6",on_press = self.add_number))
+		gl.add_widget( Button(text="-",on_press = self.add_operation) )
+
+		gl.add_widget( Button(text="1",on_press = self.add_number))
+		gl.add_widget( Button(text="2",on_press = self.add_number))
+		gl.add_widget( Button(text="3",on_press = self.add_number))
+		gl.add_widget( Button(text="+",on_press = self.add_operation) )
 
 		gl.add_widget( Widget())
-		gl.add_widget( Button(text="0"))
-		gl.add_widget( Button(text="."))
-		gl.add_widget( Button(text="="))
+		gl.add_widget( Button(text="0",on_press = self.add_number))
+		gl.add_widget( Button(text=".",on_press = self.add_number))
+		gl.add_widget( Button(text="=",on_press = self.add_operation) )
 
 		bl.add_widget(gl)
 		return bl
